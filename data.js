f9b66dc92dfa83e5d8be1cbd02d512edf0aa3c16
OmarAyman1234 <omarayman9124@gmail.com>
2024-11-20 16:51:40 +0200

    Improved the website's structure

--- data.js
+++ data.js
@@ -4,69 +4,69 @@ const books = [
     name: "Book 1",
     author: "Author 1",
     description: "Description 1",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 2,
     name: "Book 2",
     author: "Author 2",
     description: "Description 2",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 3,
     name: "Book 3",
     author: "Author 3",
     description: "Description 3",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 4,
     name: "Book 4",
     author: "Author 4",
     description: "Description 4",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 5,
     name: "Book 5",
     author: "Author 5",
     description: "Description 5",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 6,
     name: "Book 6",
     author: "Author 6",
     description: "Description 6",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 7,
     name: "Book 7",
     author: "Author 7",
     description: "Description 7",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 8,
     name: "Book 8",
     author: "Author 8",
     description: "Description 8",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 9,
     name: "Book 9",
     author: "Author 9",
     description: "Description 9",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
   {
     id: 10,
     name: "Book 10",
     author: "Author 10",
     description: "Description 10",
-    image: "images/initial-book-img.png"
+    image: "./assets/images/initial-book-img.png"
   },
 ];
