31ba0bf913cabc52105a62416ebeb36c28ea3120
DORMODO <ymb01072005@gmail.com>
2024-11-20 22:00:46 +0200

    **-q-**

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// Get the navbar toggle button and navbar
 const navbarToggle = document.querySelector('.navbar__toggle');
 const navbar = document.querySelector('.navbar');
 
-// Add an event listener to toggle the navbar visibility
 navbarToggle.addEventListener('click', () => {
-  navbar.classList.toggle('active');  // Add/remove the active class on click
+  navbar.classList.toggle('active');
 });
