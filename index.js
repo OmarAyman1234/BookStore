78229e3d1bc0f54fb1e2c205e3a93d1bbc8bf96c
DORMODO <ymb01072005@gmail.com>
2024-11-21 21:40:41 +0200

    ...

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import { generateHeader, generateFooter } from "/scripts/headerFooter.js";
-=======
-import { generateHeader, generateFooter } from "./scripts/headerFooter.js";
->>>>>>> ad92949f74822bc8581ebceb9233324c94726ccf
-
-generateHeader();
-generateFooter();
-
->>>>>>> 8a2df227e0b8dafe83684fb313de77eff23a435c
 const navbarToggle = document.querySelector('.navbar__toggle');
 const navbar = document.querySelector('.navbar');
 
@@ -17,3 +5,15 @@ navbarToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.querySelector(".btn--secondary");
+  const signUpButton = document.querySelector(".btn--primary");
+  const navbarAuth = document.querySelector(".navbar__auth");
+  const navbarProfile = document.querySelector(".navbar__profile");
+
+  // Simulate login
+  loginButton.addEventListener("click", () => {
+    navbarAuth.classList.add("logged-in"); // Add the logged-in class
+  });
+});
