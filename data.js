1355326c0082c36e1f740cc5942a3f44dbe6ff86
OmarAyman1234 <omarayman9124@gmail.com>
2024-11-21 08:05:17 +0200

    Added categories, and added 2 books

--- data.js
+++ data.js
@@ -4,27 +4,47 @@ const books = [
     name: "Riyadh As-Salihin",
     author: "An-Nawawi",
     description: "",
-    image: "./assets/images/books-data/Riyadh-As-Salihin.jpg"
+    image: "./assets/images/books-data/Riyadh-As-Salihin.jpg",
+    category: "hadith"
   },
   {
     id: 2,
     name: "Sahih Al-Bukhari",
     author: "Al-Bukhari",
     description: "",
-    image: "./assets/images/books-data/sahih-al-bukhari.jpg"
+    image: "./assets/images/books-data/sahih-al-bukhari.jpg",
+    category: "hadith"
   },
   {
     id: 3,
     name: "The Translations of the Meanings of Summarized Sahih Muslim",
     author: "Muslim",
     description: "Description 3",
-    image: "./assets/images/books-data/summarized-sahih-muslim-meanings.jpg"
+    image: "./assets/images/books-data/summarized-sahih-muslim-meanings.jpg",
+    category: "hadith"
   },
   {
     id: 4,
     name: "Forty Hadith of An-Nawawi",
     author: "An-Nawawi",
     description: "Description 4",
-    image: "./assets/images/books-data/40-Hadith-An-Nawawi.jpg"
+    image: "./assets/images/books-data/40-Hadith-An-Nawawi.jpg",
+    category: "hadith"
   },
+  {
+    id: 5,
+    name: "Tafseer Ibn Kathir Volume 2",
+    author: "Ibn Kathir",
+    description: "",
+    image: "./assets/images/books-data/Tasfir_Ibn_Kathir_Vol._2.png",
+    categoty: "tafsir"
+  },
+  {
+    id: 6,
+    name: "Tafseer As-Sa'di",
+    author: "Abdur-Rahman Naser As-Sa'di",
+    description: "",
+    image: "./assets/images/books-data/tafseer-as-sa-di-10-volume-set.jpg",
+    category: "tafsir"
+  }
 ];
